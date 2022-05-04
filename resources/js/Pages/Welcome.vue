7f3ce0b00985f8945e7d51a6745fa25168a48fda
Thomas <thomas.bockhorn@tecktonet.com>
2022-05-04 05:19:27 -0500

    Setup a new portfolio website

---
+++ resources/js/Pages/Welcome.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { Head, Link } from "@inertiajs/inertia-vue3";
+
+</script>
+
+<template>
+  <div>
+    <Head title="Welcome" />
+
+  
+  </div>
+</template>
+
+<style scoped>
+
+</style>
