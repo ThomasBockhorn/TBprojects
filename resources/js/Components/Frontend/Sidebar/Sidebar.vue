5aea3d7a7b7d17a36e2a8f4ec39a3df8548b18ac
Thomas <thomas.bockhorn@tecktonet.com>
2022-05-13 22:15:52 -0500

    Reformated sidebar code

--- resources/js/Components/Frontend/Sidebar/Sidebar.vue
+++ resources/js/Components/Frontend/Sidebar/Sidebar.vue
@@ -1,12 +1,36 @@
 <template>
-<div class="h-70 w-auto z-40 fixed bg-amber-200"> 
+  <div class="h-70 w-auto z-40 fixed bg-amber-200">
     <nav class="text-black text-center">
-       <Link class="block p-4 hover:bg-amber-400" :class="{ 'bg-amber-400': $page.url === '/'}"  href="/" preserve-scroll>Home</Link>
-       <Link class="block p-4 hover:bg-amber-400" :class="{ 'bg-amber-400': $page.url.startsWith('/projects') }" href="/projects" preserve-scroll>Projects</Link>
-       <Link class="block p-4 hover:bg-amber-400" :class="{ 'bg-amber-400': $page.url === '/contact'}" href="/contact" preserve-scroll>Contact</Link>
-       <Link class="block p-4 hover:bg-amber-400" :class="{ 'bg-amber-400': $page.url === '/resume'}" href="/resume" preserve-scroll>Resume</Link>
+      <Link
+        class="block p-4 hover:bg-amber-400"
+        :class="{ 'bg-amber-400': $page.url === '/' }"
+        href="/"
+        preserve-scroll
+        >Home</Link
+      >
+      <Link
+        class="block p-4 hover:bg-amber-400"
+        :class="{ 'bg-amber-400': $page.url.startsWith('/projects') }"
+        href="/projects"
+        preserve-scroll
+        >Projects</Link
+      >
+      <Link
+        class="block p-4 hover:bg-amber-400"
+        :class="{ 'bg-amber-400': $page.url === '/contact' }"
+        href="/contact"
+        preserve-scroll
+        >Contact</Link
+      >
+      <Link
+        class="block p-4 hover:bg-amber-400"
+        :class="{ 'bg-amber-400': $page.url === '/resume' }"
+        href="/resume"
+        preserve-scroll
+        >Resume</Link
+      >
     </nav>
-</div>
+  </div>
 </template>
 
 <script setup>
