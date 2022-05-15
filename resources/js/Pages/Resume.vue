aba8f73725fa2ac3f3b35e6b5c00f6987e0c128a
Thomas <thomas.bockhorn@tecktonet.com>
2022-05-14 20:46:31 -0500

    Added pdf resume for vistors to see

--- resources/js/Pages/Resume.vue
+++ resources/js/Pages/Resume.vue
@@ -1,18 +1,19 @@
 <template>
-    <div>
-        <Head title="Resume" />
-        <GuestLayout>
-        
-        </GuestLayout>
-    </div>
+  <div>
+    <Head title="Resume" />
+    <GuestLayout>
+      <div id="main" class="mt-20">
+        <VuePdfEmbed :page="1" :source="'/pdf/TechResume2020.pdf'" />
+      </div>
+    </GuestLayout>
+  </div>
 </template>
 
 <script setup>
-    import GuestLayout from "@/Layouts/Guest";
-    import { Head } from "@inertiajs/inertia-vue3";
-
+import GuestLayout from "@/Layouts/Guest";
+import { Head } from "@inertiajs/inertia-vue3";
+import VuePdfEmbed from "vue-pdf-embed";
 </script>
 
 <style scoped>
-
 </style>
