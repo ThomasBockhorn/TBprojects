9a066ea69272be8920d840fcce7f7c42b2474e71
Thomas Bockhorn <historian.thomas@gmail.com>
2022-05-28 10:18:50 -0500

    Changed send button color in contact

--- public/js/app.js
+++ public/js/app.js
@@ -27128,7 +27128,7 @@ var _hoisted_7 = {
 
 var _hoisted_8 = /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("input", {
   type: "submit",
-  "class": "w-full px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out",
+  "class": "w-full px-6 py-2.5 bg-amber-200 text-black font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-amber-400 hover:shadow-lg focus:bg-amber-400 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-amber-400 active:shadow-lg transition duration-150 ease-in-out",
   value: "Send"
 }, null, -1
 /* HOISTED */
--- resources/js/Pages/Contact.vue
+++ resources/js/Pages/Contact.vue
@@ -105,20 +105,20 @@
                   w-full
                   px-6
                   py-2.5
-                  bg-blue-600
-                  text-white
+                  bg-amber-200
+                  text-black
                   font-medium
                   text-xs
                   leading-tight
                   uppercase
                   rounded
                   shadow-md
-                  hover:bg-blue-700 hover:shadow-lg
-                  focus:bg-blue-700
+                  hover:bg-amber-400 hover:shadow-lg
+                  focus:bg-amber-400
                   focus:shadow-lg
                   focus:outline-none
                   focus:ring-0
-                  active:bg-blue-800 active:shadow-lg
+                  active:bg-amber-400 active:shadow-lg
                   transition
                   duration-150
                   ease-in-out
